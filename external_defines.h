ce074328630f4d24a040e48bea91b882bf94895a
Martin Sperl <kernel@martin.sperl.org>
2016-05-25 09:05:16 +0000

    Newer version that: * adds descriptions and notes * fixes building
    Note that there seems to be a different version of
    Brcm_Android_ICS_Graphics_Stack.tar.gz that can get downloaded
    now compared to when it came out.

---
+++ external_defines.h
@@ -0,0 +1,15 @@
+/* here go some external defines that are not in the BRCM tarball */
+
+/* Descriptions for distinct Register ranges */
+#if 0
+#define CM_DESCRIPTION		"Clock manager"
+#define A2W_DESCRIPTION		"Clock manager PLL control"
+#define APHY_CSR_DESCRIPTION	"SDRAM Adress (pin) control"
+#define DPHY_CSR_DESCRIPTION	"SDRAM Data (pin) control"
+#define SD_DESCRIPTION		"SDRAM"
+#define L1_DESCRIPTION		"VC4-L1 control"
+#define L2_DESCRIPTION		"VC4-L2 control"
+#define PM_DESCRIPTION		"Power manager"
+#define OTP_DESCRIPTION		"One Time programmable"
+#define GP_DESCRIPTION		"GPIO control"
+#endif
